1d1c8b6ba41c1296b6049b74a9b2140369e7fb22
Sebastian Willenbrink <sewi@linux.fritz.box>
2021-08-15 15:19:17 +0200

    Move block data into one array for internal rep

--- ftd_rep.ml
+++ ftd_rep.ml
@@ -1,4 +1,5 @@
 open Util
+open Common
 
 type version = {
   major : int [@key "Major"];
@@ -6,7 +7,7 @@ type version = {
 } [@@deriving show, yojson]
 let version_of_yojson t = [%of_yojson: version] t |> check (fun v -> v.major = 1 && v.minor = 0)
 
-type item_dictionary = (int * Common.guid) list [@@deriving show]
+type item_dictionary = (int * guid) list [@@deriving show]
 let item_dictionary_of_yojson t = Yo.Util.to_assoc t |> List.map (fun (id,x) -> (int_of_string id, string_of_yojson x))
 let yojson_of_item_dictionary t = `Assoc (List.map (fun (id,x) -> (string_of_int id, `String x)) t)
 
@@ -14,14 +15,16 @@ let yojson_of_item_dictionary t = `Assoc (List.map (fun (id,x) -> (string_of_int
 type author_details = {
   valid : bool [@key "Valid"];
   foreign_blocks : int [@key "ForeignBlocks"];
-  creator_id : string [@key "CreatorId"];
-  object_id : string [@key "ObjectId"];
+  creator_id : guid [@key "CreatorId"];
+  object_id : guid [@key "ObjectId"];
   creator_readable_name : string [@key "CreatorReadableName"];
   hash_v1 : string [@key "HashV1"];
 } [@@deriving show, yojson]
 
 type csi = float array [@@deriving show]
-let csi_of_yojson t = [%of_yojson: float list] t |> Array.of_list
+(* Dependant on the version. From 3.0 onwards it is 80 *)
+let invar_csi t = check (fun x -> Array.length x = 65 || Array.length x = 70 || Array.length x = 80) t
+let csi_of_yojson t = [%of_yojson: float list] t |> Array.of_list |> invar_csi
 let yojson_of_csi t = Array.to_list t |> [%yojson_of: float list]
 
 (* Possible related to subobjects? Only 4 models parse without option. *)
@@ -33,24 +36,16 @@ let yojson_of_col t =
   |> Option.map Array.to_list
   |> [%yojson_of: v4 list option]
 
-type blp = v3i array [@@deriving show]
-let blp_of_yojson t = [%of_yojson: v3i list] t |> Array.of_list
-let yojson_of_blp t = Array.to_list t |> [%yojson_of: v3i list]
-
-type blr = int array [@@deriving show]
-let blr_of_yojson t = [%of_yojson: int list] t |> Array.of_list
-let yojson_of_blr t = Array.to_list t |> [%yojson_of: int list]
-
 type blueprint = {
   material_contained : float [@key "ContainedMaterialCost"];
   csi : csi [@key "CSI"] [@opaque];
   col : col [@key "COL"] [@opaque];
   scs : blueprint list [@key "SCs"];
-  blp : blp [@key "BLP"];
-  blr : blr [@key "BLR"];
+  blp : v3i list [@key "BLP"];
+  blr : int list [@key "BLR"];
   bp1 : Yo.t [@key "BP1"];
   bp2 : Yo.t [@key "BP2"];
-  block_colors : int list [@key "BCI"] [@opaque];
+  bci : int list [@key "BCI"] [@opaque];
   bei : Yo.t [@key "BEI"];
   block_data : Yo.t [@key "BlockData"] [@opaque];
   vehicle_data : Yo.t [@key "VehicleData"] [@opaque];
@@ -76,10 +71,11 @@ type blueprint = {
   persistent_block_index : int [@key "PersistentBlockIndex"];
   author_details : author_details [@key "AuthorDetails"] [@opaque];
   block_count : int [@key "BlockCount"];
-  (* id : int [@key "Id"];
-   * last_alive_block : int [@key "LastAliveBlock"];
-   * index_of_first_block_needing_full_repair_cost : int [@key "IndexOfFirstBlockNeedingFullRepairCost"]; *)
 } [@@deriving show, yojson]
+let blueprint_of_yojson t =
+  let r = [%of_yojson: blueprint] t in
+  r
+  |> check List.(fun r -> let bc = r.block_count in bc = length r.blp && bc = length r.blr && bc = length r.block_ids && bc = length r.bci)
 
 type construct = {
   file_model_version : version [@key "FileModelVersion"];
@@ -93,20 +89,23 @@ type construct = {
 } [@@deriving show, yojson]
 
 let parse_construct t =
-  (try
-    Yo.Util.(
-      t
-      |> member "Blueprint"
-      |> member "GameVersion"
-      |> to_string
-      |> (fun str -> str,
-                     try match str |> String.split_on_char '.' |> List.map int_of_string with
-                         (3::_) -> true
-                       | (2::minor::_) -> minor >= 7
-                       | (1::_) -> false
-                       | _ -> false
-                     with _ -> false)
-      |> (fun (str,supported) -> if not supported then raise (Unsupported_Version str) else ())
-    )
-  with _ -> raise (Unsupported_Version "Error getting version"));
+  let version =
+    try
+      Yo.Util.(
+        t
+        |> member "Blueprint"
+        |> member "GameVersion"
+        |> to_string
+        |> (fun str -> str,
+                       try match str |> String.split_on_char '.' |> List.map int_of_string with
+                           (3::_) -> true
+                         | (2::minor::_) -> minor >= 7
+                         | (1::_) -> false
+                         | _ -> false
+                       with _ -> false)
+        |> (fun (str,supported) -> if not supported then raise (Unsupported_Version str) else str)
+      )
+    with Yo.Util.Type_error ("Expected string, got null", _) -> raise (Unsupported_Version "Error getting version")
+  in
+  (if !Cli.verbose then Printf.printf "Blueprint version: %s\n" version);
   construct_of_yojson t
--- int_rep.ml
+++ int_rep.ml
@@ -9,7 +9,7 @@ type rotation =
   | Down
   | Unknown of int
 [@@deriving show]
-let rotation_of_yojson t = match [%of_yojson: int] t with
+let rotation_of_int t = match t with
   | 0 -> Forward
   | 1 -> Right
   | 2 -> Back
@@ -38,95 +38,127 @@ let rotation_of_yojson t = match [%of_yojson: int] t with
   | 7 -> Down
   | 11 -> Up
   | x -> Printf.fprintf stderr "Unknown rotation: %i.\n" x; Unknown x
-type version = {
-  major : int [@key "Major"];
-  minor : int [@key "Minor"]
-} [@@deriving show, yojson]
+
+let int_of_rotation t = 0
 
 type item_dictionary = (int * Common.guid) list [@@deriving show]
-let item_dictionary_of_yojson t = Yo.Util.to_assoc t |> List.map (fun (id,x) -> (int_of_string id, string_of_yojson x))
-let yojson_of_item_dictionary t = `Assoc (List.map (fun (id,x) -> (string_of_int id, `String x)) t)
-
-(* GUIDs are IDs used everywhere and unique to everything. That means, authors and crafts share the same id type. *)
-type author_details = {
-  valid : bool [@key "Valid"];
-  foreign_blocks : int [@key "ForeignBlocks"];
-  creator_id : string [@key "CreatorId"];
-  object_id : string [@key "ObjectId"];
-  creator_readable_name : string [@key "CreatorReadableName"];
-  hash_v1 : string [@key "HashV1"];
-} [@@deriving show, yojson]
 
 type csi = float array [@@deriving show]
-let csi_of_yojson t = [%of_yojson: float list] t |> Array.of_list
-let yojson_of_csi t = Array.to_list t |> [%yojson_of: float list]
 
 (* Possible related to subobjects? Only 4 models parse without option. *)
 type col = v4 array option [@@deriving show]
-let invar_col t = Option.map (check (fun x -> Array.length x = 32)) t
-let col_of_yojson t = [%of_yojson: v4 list option] t |> Option.map Array.of_list |> invar_col
-let yojson_of_col t =
-  invar_col t
-  |> Option.map Array.to_list
-  |> [%yojson_of: v4 list option]
 
 type blp = v3i array [@@deriving show]
-let blp_of_yojson t = [%of_yojson: v3i list] t |> Array.of_list
-let yojson_of_blp t = Array.to_list t |> [%yojson_of: v3i list]
 
 type blr = int array [@@deriving show]
-let blr_of_yojson t = [%of_yojson: int list] t |> Array.of_list
-let yojson_of_blr t = Array.to_list t |> [%yojson_of: int list]
 
-type blueprint = {
-  material_contained : float [@key "ContainedMaterialCost"];
-  csi : csi [@key "CSI"] [@opaque];
-  col : col [@key "COL"] [@opaque];
-  scs : blueprint list [@key "SCs"];
-  blp : blp [@key "BLP"];
-  blr : blr [@key "BLR"];
-  bp1 : Yo.t [@key "BP1"];
-  bp2 : Yo.t [@key "BP2"];
-  block_colors : int list [@key "BCI"] [@opaque];
-  bei : Yo.t [@key "BEI"];
-  block_data : Yo.t [@key "BlockData"] [@opaque];
-  vehicle_data : Yo.t [@key "VehicleData"] [@opaque];
-  design_changed : bool [@key "designChanged"];
-  blueprint_version : int [@key "blueprintVersion"];
-  blueprint_name : string option [@key "blueprintName"];
-  serialised_info : Yo.t [@key "SerialisedInfo"];
-  name : string option [@key "Name"];
-  item_number : int [@key "ItemNumber"];
-  local_position : v3 [@key "LocalPosition"];
-  local_rotation : v4 [@key "LocalRotation"];
-  force_id : int [@key "ForceId"];
-  total_block_count : int [@key "TotalBlockCount"];
-  max_cords : v3i [@key "MaxCords"];
-  min_cords : v3i [@key "MinCords"];
-  block_ids : int list [@key "BlockIds"];
-  block_state : Yo.t [@key "BlockState"];
-  alive_count : int [@key "AliveCount"];
-  block_string_data : Yo.t [@key "BlockStringData"];
-  block_string_data_ids : Yo.t [@key "BlockStringDataIds"];
-  game_version : string [@key "GameVersion"];
-  persistent_sub_object_index : int [@key "PersistentSubObjectIndex"];
-  persistent_block_index : int [@key "PersistentBlockIndex"];
-  author_details : author_details [@key "AuthorDetails"] [@opaque];
-  block_count : int [@key "BlockCount"];
-  (* id : int [@key "Id"];
-   * last_alive_block : int [@key "LastAliveBlock"];
-   * index_of_first_block_needing_full_repair_cost : int [@key "IndexOfFirstBlockNeedingFullRepairCost"]; *)
-} [@@deriving show, yojson]
+type block = {id : int; pos : v3i; rot : rotation; color : int; } [@@deriving show]
+
+type blueprint_int = {
+  name : string option;
+  blueprint_name : string option;
+  blueprint_version : int;
+  blocks : block array;
+  scs : blueprint_int list;
+  material_contained : float;
+  csi : csi;
+  col : col;
+  bp1 : Yo.t;
+  bp2 : Yo.t;
+  bei : Yo.t;
+  block_data : Yo.t;
+  vehicle_data : Yo.t;
+  design_changed : bool;
+  serialised_info : Yo.t;
+  item_number : int;
+  local_position : v3;
+  local_rotation : v4;
+  force_id : int;
+  total_block_count : int;
+  max_cords : v3i;
+  min_cords : v3i;
+  block_state : Yo.t;
+  alive_count : int;
+  block_string_data : Yo.t;
+  block_string_data_ids : Yo.t;
+  game_version : string;
+  persistent_sub_object_index : int;
+  persistent_block_index : int;
+  author_details : Ftd_rep.author_details;
+} [@@deriving show]
 
 type construct_int = {
-  file_model_version : version [@key "FileModelVersion"];
-  name : string option [@key "Name"];
-  version : int [@key "Version"];
-  saved_total_block_count : int [@key "SavedTotalBlockCount"];
-  saved_material_cost : float [@key "SavedMaterialCost"];
-  contained_material_cost : float [@key "ContainedMaterialCost"];
-  item_dictionary : item_dictionary [@key "ItemDictionary"];
-  blueprint : blueprint [@key "Blueprint"];
-} [@@deriving show, yojson]
+  name : string option;
+  file_model_version : Ftd_rep.version;
+  version : int;
+  saved_total_block_count : int;
+  saved_material_cost : float;
+  contained_material_cost : float;
+  item_dictionary : item_dictionary;
+  blueprint : blueprint_int;
+} [@@deriving show]
 
-let internal_of_construct t = t
+let rec blueprint_int_of_blueprint ({
+  name; blueprint_name; blueprint_version; material_contained;
+  csi; col; scs; blp; blr; bp1; bp2; bci; bei;
+  block_data; vehicle_data; design_changed; serialised_info;
+  item_number; local_position; local_rotation; force_id;
+  total_block_count; max_cords; min_cords; block_ids;
+  block_state; alive_count; block_string_data; block_string_data_ids;
+  game_version; persistent_sub_object_index; persistent_block_index;
+  author_details; block_count;
+} : Ftd_rep.blueprint) = {
+  name; blueprint_name; blueprint_version; material_contained;
+  csi; col;
+  scs = List.map blueprint_int_of_blueprint scs;
+  blocks = Array.init block_count List.(fun i -> {
+        id = nth block_ids i;
+        pos = nth blp i;
+        rot = rotation_of_int (nth blr i);
+        color = nth bci i;
+      });
+  bp1; bp2; bei; block_data; vehicle_data;
+  design_changed; serialised_info; item_number; local_position;
+  local_rotation; force_id; total_block_count; max_cords; min_cords;
+  block_state; alive_count; block_string_data;
+  block_string_data_ids; game_version; persistent_sub_object_index;
+  persistent_block_index; author_details;
+}
+
+let rec blueprint_of_blueprint_int ({
+  name; blueprint_name; blueprint_version; material_contained;
+  csi; col; scs; blocks; bp1; bp2; bei; block_data;
+  vehicle_data; design_changed; serialised_info; item_number;
+  local_position; local_rotation; force_id; total_block_count;
+  max_cords; min_cords; block_state; alive_count;
+  block_string_data; block_string_data_ids; game_version;
+  persistent_sub_object_index; persistent_block_index; author_details;
+} : blueprint_int) : Ftd_rep.blueprint =
+  {
+    material_contained; csi; col;
+    scs = List.map blueprint_of_blueprint_int scs;
+    blp = List.init (Array.length blocks) (fun i -> blocks.(i).pos);
+    blr = List.init (Array.length blocks) (fun i -> int_of_rotation (blocks.(i).rot));
+    bci = List.init (Array.length blocks) (fun i -> blocks.(i).color);
+    block_ids = List.init (Array.length blocks) (fun i -> blocks.(i).id);
+    block_count = Array.length blocks;
+    bp1; bp2;
+    bei; block_data; vehicle_data; design_changed;
+    blueprint_version; blueprint_name; serialised_info;
+    name; item_number; local_position; local_rotation;
+    force_id; total_block_count; max_cords; min_cords;
+    block_state; alive_count; block_string_data;
+    block_string_data_ids; game_version; persistent_sub_object_index;
+    persistent_block_index; author_details;
+}
+
+let internal_of_construct ({name; version; file_model_version; saved_total_block_count; saved_material_cost; contained_material_cost; item_dictionary; blueprint;} : Ftd_rep.construct) = {
+  name;
+  version;
+  file_model_version;
+  saved_total_block_count;
+  saved_material_cost;
+  contained_material_cost;
+  item_dictionary;
+  blueprint = blueprint_int_of_blueprint blueprint;
+  }
